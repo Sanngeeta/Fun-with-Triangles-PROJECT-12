d32319f036745e7ae0d1fe1fe14fbcf9252a1074
Sanngeeta <sangeetap20@navgurukul.org>
2021-11-26 11:58:01 +0530

    DOCS: FILES CREATED

---
+++ hypotenuse.js
@@ -0,0 +1,10 @@
+const sideInputs=document.querySelectorAll(".side-input")
+const hypotenuseButton=document.querySelector("#Calculate-btn")
+const showOutput=document.querySelector("#output")
+
+
+function calculateHypotenuse(){
+    console.log("clicked")
+}
+
+hypotenuseButton.addEventListener("click",calculateHypotenuse)
